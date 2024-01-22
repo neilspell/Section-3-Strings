712d8f2c223d99aacee6e130155694e7d5ea529c
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 15:41:53 +0000

    Add files via upload

---
+++ 3.3 Strings - Indexing & Substrings/main.py
@@ -0,0 +1,10 @@
+# -----------------------------------
+# Title: String Indexing & Substrings
+# Date:
+# -----------------------------------
+
+
+# Don't forget to add comments to your code
+# Comments can help explain / remind you what new lines or functions are doing
+
+
